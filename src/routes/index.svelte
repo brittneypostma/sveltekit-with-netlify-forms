46b5fd45de59319905bd6cad4706d841abff0bd6
brittneypostma <bdesignedwebdev@gmail.com>
2022-07-22 11:39:12 -0400

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form name="netlify-form-example" method="post" netlify netlify-honeypot="bot-field">
+	<input type="hidden" name="netlify-form-example" value="netlify-form-example" />
+	<input type="text" name="bot-field" />
+	<p>
+		<label>Your Name: <input type="text" name="name" /></label>
+	</p>
+	<p>
+		<label>Your Email: <input type="email" name="email" /></label>
+	</p>
+	<p>
+		<label>Message: <textarea name="message" /></label>
+	</p>
+	<p>
+		<button type="submit">Send</button>
+	</p>
+</form>
