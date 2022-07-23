1dac3bcfd438a4b7cfdcc0a0f2af4de3222a94d9
brittneypostma <bdesignedwebdev@gmail.com>
2022-07-22 20:14:46 -0400

    better form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,34 +2,9 @@
 	export const prerender = true;
 </script>
 
-<!-- <script lang="ts">
-	let isSubmitting: boolean = false;
-
-	const handleSubmit = (e: Event) => {
-		const form: HTMLFormElement | null = document.querySelector('form');
-		const formData: FormData = new FormData(e.target as HTMLFormElement);
-		let stringData: string = formData.toString();
-		isSubmitting = true;
-		return fetch('/', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: new URLSearchParams(stringData)
-		})
-			.then(() => {
-				console.log('Form successfully submitted');
-				isSubmitting = false;
-				form?.reset();
-			})
-			.catch((error) => {
-				alert(error);
-				isSubmitting = false;
-			});
-	};
-</script> -->
-
 <form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
 	<input type="hidden" name="form-name" value="netlify-form-example" />
-	<input name="name" required placeholder="Name" type="name" />
+	<input name="name" required placeholder="Name" type="text" />
 	<br />
 	<input name="email" required placeholder="Email" type="email" />
 	<br />
